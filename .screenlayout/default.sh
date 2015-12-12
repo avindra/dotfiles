bc77566083cd0b7b251113730d902e7521a789fc
Avindra Goolcharan <aavindraa@gmail.com>
2015-12-12 14:10:32 -0500

    screenlayout

---
+++ .screenlayout/default.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-1 --off --output DisplayPort-0 --off --output DVI-0 --primary --mode 1920x1080 --pos 0x360 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 1920x0 --rotate left
