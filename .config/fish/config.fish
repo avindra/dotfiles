bcba0d0c6ba979f4424ec395fad4346407e41e66
Avindra Goolcharan <aavindraa@gmail.com>
2020-11-13 18:30:06 -0500

    🐟 Switch to Fish shell
    It's about the time for a better interactive
    shell experience. We can do better than Apple,
    we can do better than ZSH. The "leap year" of
    the shell is best modeled by Fish.
    
    I still use Bash, but want to use it only
    secondarily. I used Bash from 2016-2020
    primarily, only because of wanting to keep
    memory fresh of how to work with legacy
    systems with only a POSIX/Bash console.
    
     * Mod+Enter now spawns fish shells.
     * Mod+Shift+Enter added for default
       (99.99% Bash in today's systems)
    
    Fish is better most immediately because
    of how it deals with aliases. The `abbr`
    feature alone makes Fish compelling for
    the daily shell user.
    
    Added a `_setup` script which for now,
    only contains the fisher setup.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,22 @@
-set fish_greeting "The quieter you become, the more you are able to hear."
+set fish_greeting "The Quieter you Become, the More you are able to Hear."
+
+abbr -a -g l ls -ltrah
+abbr -a -U pbcopy xclip -selection c
+
+# git aliases
+abbr -a -g g git
+abbr -a -g s git status -uno
+abbr -a -g a git add -u
+abbr -a -g r git reset HEAD
+abbr -a -g p git pull --rebase
+abbr -a -g P git push
+abbr -a -g H git show HEAD
+abbr -a -g x git checkout --
+abbr -a -g c git commit -v
+abbr -a -g C git commit --amend --no-edit
+
+# check entropy
+abbr -a -g S cat /proc/sys/kernel/random/entropy_avail
+
+# super diff
+# todo: port
