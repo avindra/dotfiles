adefe29825ab1b2716ab9a6f9359f396930ce9ba
Avindra Goolcharan <aavindraa@gmail.com>
2026-02-22 23:55:27 -0500

    wezterm: port to windows

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,23 @@
 
 -- https://wezfurlong.org/wezterm/config/files.htm
 
-return {
-	font_size = 22.0,
-	window_decorations = "NONE",
-	check_for_updates = false,
-	enable_tab_bar = false,
+local wezterm = require 'wezterm'
+
+local config = {
+	check_for_updates = false
 }
+
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+	config.font_size = 18.0
+	config.window_decorations = "INTEGRATED_BUTTONS|RESIZE|TITLE"
+	config.show_tabs_in_tab_bar = true
+	config.use_fancy_tab_bar = true
+	config.show_new_tab_button_in_tab_bar = true
+	config.enable_tab_bar = true
+else
+	config.font_size = 22.0
+	config.window_decorations = "NONE"
+end
+
+
+return config
